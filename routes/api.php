ca90816e14bc2d3f48cefda4111ddccfa0586c2d
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-09-09 19:22:37 +0800

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:api');
